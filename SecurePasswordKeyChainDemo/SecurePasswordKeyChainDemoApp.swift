64c29d6b0195a647627aff6ed1ef3a209e08407a
Ravikanth Marri <ravikanth.marri@gmail.com>
2024-03-09 10:47:38 +0000

    Initial Commit

---
+++ SecurePasswordKeyChainDemo/SecurePasswordKeyChainDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurePasswordKeyChainDemoApp.swift
+//  SecurePasswordKeyChainDemo
+//
+//  Created by Ravikanth on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SecurePasswordKeyChainDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
